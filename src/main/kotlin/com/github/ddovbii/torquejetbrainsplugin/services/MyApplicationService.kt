9e064d41069317a53a3d978650ea1f81b2bf31d0
GitHub Action <action@github.com>
2022-05-24 07:16:17 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ddovbii/torquejetbrainsplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ddovbii.torquejetbrainsplugin.services
+
+import com.github.ddovbii.torquejetbrainsplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/ddovbii/torquejetbrainsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ddovbii/torquejetbrainsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ddovbii.torquejetbrainsplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ddovbii.torquejetbrainsplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
